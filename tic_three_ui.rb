f5df95d233b9fb911240f50d7d4e5f6370bb4d52
Marcie Morton & Dorrit Geshuri <dorritgeshuri@gmail.com>
2013-02-25 18:02:30 -0800

    working on not overwriting a previous move

--- tic_three_ui.rb
+++ tic_three_ui.rb
@@ -10,16 +10,21 @@ until board.over?
     puts "\n\n#{board.display}\n\n\n"
     puts "#{player.name} it's your turn. Choose a spot by typing in the number."
     player_choice = gets.chomp
-
-    if board.space_marked?(player_choice) == false
-      board.change_mark(player_choice, player.name)
-    else
+    until @spaces[player_choice] != 'X' || @spaces[player_choice] != 'O'
       puts "That space is marked. Choose another."
       player_choice = gets.chomp
-      board.change_mark(player_choice, player.name)
     end
+    board.change_mark(player_choice, player.name) 
   end
 
  # "#{player.name}, you win!"
  
 end
+
+
+# def space_marked?(player_choice)
+#   until @spaces[player_choice] != 'X' || @spaces[player_choice] != 'O'
+#     puts "That space is marked. Choose another."
+#     player_choice = gets.chomp
+#   end
+# end
